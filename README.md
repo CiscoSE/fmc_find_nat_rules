5328072bb8c0cb198fead0eaef9a27ba2381c298
kdhurjaty <39620870+kdhurjaty@users.noreply.github.com>
2019-11-05 23:05:00 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,86 @@
+# FMC_Find_NAT_Ruls.py
+
+*Finds the NAT Rules by IP Address or Network Groups*
+
+---
+
+## Motivation
+As of now ther is no option avialable on FMC for searching the NAT Rules. So Admin has to go page by page to search the rule. In complex environments it might be difficult to find the same. This scripts saves the time by searching the NAT Rules.
+
+## Show Me!
+
+Sample use of script with output.
+------------------------------------------------------------------------------------------------
+Python3 FTD_Find_NAT_Rules FMC_IPAddress usename password i IPAddress
+
+Connected Successfully 
+
+Domain UUID IS: e276abec-e0f2-11e3-8169-6d9ed49b625f
+
+GET successful. Loading Network Objects --> 
+
+GET successful. Response data --> 
+
+GET successful for NAT Policy --> FTD-Mig-1554755781
+ 
+
+ NAT Rule Match at line number 16
+
+{
+    "destinationInterface": {
+        "id": "SampleCodeGrp",
+        "type": "InterfaceGroup"
+    },
+    "dns": false,
+
+-----------------------------------------------------------------------------------------------------
+
+## Features
+
+- Search can be based on the IP Address or Network Group.
+- The search paremeter is considered as Original Source / source Group in FTD NAT rules and Original network in the Auto NAT rules.
+- Scripts considers all instances of IP Addresses and Groups while searching the NAT Rules. In FMC Multiple Network Objects can be created with the    same IP Address and same Network Object can be used in Multiple Network Groups.
+- All NAT plocicies are searched for individual NAT Rules for the match. If Match is found scripts returns the line number and NAT Rule as well. 
+- Script is written and tested for single domain deployment of FMC.
+
+## Technologies & Frameworks Used
+
+- FMC APIs are used.
+
+- NO Third-Party products or Services are used.
+
+- Script is written in Python 3
+
+## Usage
+Script will take 5 parameters. 
+
+1. IPAddressofFMC - IP Address of FMC device. 
+2. Username - username of FMC
+3. Password - Password of FMC
+4. switch g/i - If user wants to search with IP Address use "i" as a switch.
+                If user wants to search with Network Group use "g" switch.
+
+5. Name/IP address - Name of the Netwrok group or IP Address to search the NAT rules.
+
+## Installation
+
+Execute the script as any other Python script form console. Check the reachbility to FMC. Script is tested on FMC 6.3.
+
+## Authors & Maintainers
+
+This is my first script in PYthon, contact me for any modifications on this script.
+
+- Niranjan Kiran Kumar Dhurjaty <nidhurja@cisco.com>
+
+## Extensions
+
+- Script can be extended in the following ways.
+- Search can be restricted to first 3 octets of IPv4 address, so that user may get more flexibility.
+- Search and list all groups with IP address provided in switch “I”. This will eliminate the requirement of “Group name search”.
+
+## License
+
+This project is licensed to you under the terms of the [Cisco Sample
+Code License](./LICENSE).
+
+
---
+++ fmc_find_nat_rules.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""FMC_Find_NAT_Rules Console Script.
+
+Copyright (c) 2019 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+"""
+
+__author__ = "Niranjan Kiran Kumar Dhurjaty"
+__email__ = "nidhurja@cisco.com"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2019 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+
+#
+# Generated FMC REST API sample script
+# This Script is only to search through NAT rules for given IP ADDRESS.
+# Multi Domain is not considered on this script. Please use the domain id field for the same.
+#
+
+import json
+import sys
+import requests
+
+from urllib3.exceptions import InsecureRequestWarning
+
+
+############################################################################################
+#                                                                                          #
+# Pull all NAT rules in a NAT policy and match the object ID with the ID LIST prepared     #
+# Based on the IP Address for search                                                       #
+############################################################################################
+
+
+def Find_n_Match_NatRule(objectid,PolicyName):
+  global ObjtoFind
+
+  api_path = "/policy/ftdnatpolicies/"  + objectid + "/natrules?expanded=true&limit=1000"
+  url = server + api_path_fixed + DomainUUID + api_path
+  if (url[-1] == '/'):
+      url = url[:-1]
+  try:
+    s = requests.get(url, headers=headers, verify=False)
+    status_code = s.status_code
+    resp = s.text
+    if (status_code == 200):
+       print("\n\nGET successful for NAT Policy --> " + PolicyName + "\n")
+       json_resp_local = json.loads(resp)
+       RuleNumber = 1
+       if ( json_resp_local['paging']['count'] == 0):
+           print("No matching Rules in this Policy\n\n")
+           if s : s.close()
+       else:
+           for j in json_resp_local['items']:
+              if (j['type'] == 'FTDManualNatRule'):
+                for a in NetworkObjList:
+                  if (a['Obj'] == j['originalSource']['id']):
+                    print(" \n\n NAT Rule Match at line number " + str(RuleNumber) + "\n\n")
+                    print(json.dumps(j,sort_keys=True,indent=4, separators=(',', ': ')))
+              else:
+                for a in NetworkObjList:
+                  if (a['Obj'] == j['originalNetwork']['id']):
+                    print(" \n\n NAT Rule Match at line number " + str(RuleNumber) + "\n\n")
+                    print(json.dumps(j,sort_keys=True,indent=4, separators=(',', ': ')))
+
+              RuleNumber = RuleNumber + 1
+    else:
+       s.raise_for_status()
+       print("Error occurred in GET --> "+resp)
+  except requests.exceptions.HTTPError as err:
+    print ("Error in connection --> "+str(err))
+  finally:
+   if s : s.close()
+
+   ############################################################################################
+   #                                                                                          #
+   # Prepare a list of network objects and groups matching the IP ADDRESS in the arguments    #
+   #                                                                                          #
+   ############################################################################################
+
+def FindNetworkObj(api_path):
+  global ObjtoFind
+  global NetworkObjList
+
+  try:
+       r = requests.get(url, headers=headers, verify=False)
+       status_code = r.status_code
+       resp = r.text
+       if (status_code == 200):
+          print("\n\nGET successful. Loading Newwork Objects --> \n\n")
+          json_resp = json.loads(resp)
+          for i in json_resp['items']:
+
+            if IPAddToFind == i['value']:
+              NetworkObj['name']= i['name']
+              NetworkObj['Obj']= i['id']
+              ObjtoFind = NetworkObj
+              NetworkObjList.append(NetworkObj.copy())
+       else:
+          r.raise_for_status()
+          print("Error occurred in GET --> "+resp)
+  except requests.exceptions.HTTPError as err:
+       print ("Error in connection --> "+str(err))
+  finally:
+      if r : r.close()
+
+      ############################################################################################
+      #                                                                                          #
+      # Prepare a list of network groups matching the IP ADDRESS in the arguments                #
+      #                                                                                          #
+      ############################################################################################
+def FindNetworkGrp(api_path):
+      global ObjtoFind
+      global NetworkObjList
+      #global NameToFind
+
+      try:
+          r = requests.get(url, headers=headers, verify=False)
+          status_code = r.status_code
+          resp = r.text
+          if (status_code == 200):
+             print("\n\nGET successful. Loading Newwork Groups --> \n\n")
+             json_resp = json.loads(resp)
+             for i in json_resp['items']:
+               if (NameToFind == i['name']):
+                 print (i['name'])
+                 NetworkObj['name']= i['name']
+                 NetworkObj['Obj']= i['id']
+                 ObjtoFind = NetworkObj
+                 NetworkObjList.append(NetworkObj.copy())
+          else:
+             r.raise_for_status()
+             print("Error occurred in GET --> "+resp)
+      except requests.exceptions.HTTPError as err:
+          print ("Error in connection --> "+str(err))
+      finally:
+        if r : r.close()
+
+      ############################################################################################
+      #                                                                                          #
+      # Main Code Starts Here. Execution starts here.                                            #
+      #                                                                                          #
+      ############################################################################################
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+server = ""
+NetworkObjList = [{'name':'none','Obj':'None'}]
+NetworkObj= {'name':'none','Obj':'None'}
+ObjtoFind = {'name':'none','Obj':'None'}
+api_path_fixed ="/api/fmc_config/v1/domain/"
+
+count = 1
+username = "admin"
+if len(sys.argv) > 1:
+    server = "https://" + sys.argv[1]
+if len(sys.argv) > 2:
+    username = sys.argv[2]
+password = "sf"
+if len(sys.argv) > 3:
+    password = sys.argv[3]
+if len(sys.argv) > 4:
+    WhatToFine = sys.argv[4]
+if (WhatToFine == 'g'):
+  NameToFind = sys.argv[5]
+else:
+  IPAddToFind = sys.argv[5]
+print ("\n Generating Token and Connecting \n\n")
+r = None
+headers = {'Content-Type': 'application/json'}
+api_auth_path = "/api/fmc_platform/v1/auth/generatetoken"
+auth_url = server + api_auth_path
+try:
+      r = requests.post(auth_url, headers=headers, auth=requests.auth.HTTPBasicAuth(username,password), verify=False)
+      auth_headers = r.headers
+      auth_token = auth_headers.get('X-auth-access-token', default=None)
+      if auth_token == None:
+        print("auth_token not found. Exiting...")
+        sys.exit()
+except Exception as err:
+    print ("Error in generating auth token --> "+str(err))
+    sys.exit()
+
+headers['X-auth-access-token']=auth_token
+print ("\nConnected Successfully \n")
+DomainUUID=auth_headers['DOMAIN_UUID']
+print ("\n\nDomain UUID IS: " + DomainUUID) # Check for the Domain Id and replace the URLs.
+if (WhatToFine=='i'):
+  api_path = "/object/networkaddresses?expanded=True&limit=1000"
+  url = server + api_path_fixed + DomainUUID + api_path
+  if (url[-1] == '/'):
+    url = url[:-1]
+  FindNetworkObj(api_path)
+else:
+  if (WhatToFine == 'o'):
+    api_path = "/object/networkgroups?expanded=True&limit=1000"
+    url = server + api_path_fixed + DomainUUID + api_path
+    if (url[-1] == '/'):
+      url = url[:-1]
+    FindNetworkGrp(api_path)
+  else:
+    sys.exit()
+
+api_path = "/policy/ftdnatpolicies?expanded=True&limit=1000"
+url = server + api_path_fixed + DomainUUID + api_path
+if (url[-1] == '/'):
+    url = url[:-1]
+try:
+     r = requests.get(url, headers=headers, verify=False)
+     status_code = r.status_code
+     resp = r.text
+     if (status_code == 200):
+         print("GET successful. Response data --> ")
+         json_resp = json.loads(resp)
+         for i in json_resp['items']:
+             Find_n_Match_NatRule ( i['id'],i['name'])
+
+     else:
+         r.raise_for_status()
+         print("Error occurred in GET --> "+resp)
+except requests.exceptions.HTTPError as err:
+     print ("Error in connection --> "+str(err))
+finally:
+     if r : r.close()
